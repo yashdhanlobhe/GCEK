7ada8cb2c46f63d1fe7c5bbe9dea05832346c0ff
billion-yash <billionyash@gmail.com>
2020-12-16 18:49:45 +0530

    Initial commit

---
+++ gcekhost/src/main/java/com/example/gcekhost/Notification/Notifcation.java
@@ -0,0 +1,47 @@
+package com.example.gcekhost.Notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.gcekhost.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Notifcation extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    Button send_btn;
+    EditText notification_title , notification_description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifcation);
+        // Write a message to the database
+
+        database = FirebaseDatabase.getInstance();
+
+
+        send_btn = (Button)findViewById(R.id.send_btn_notifiacaiton);
+        notification_title = (EditText) findViewById(R.id.title_notification);
+        notification_description=(EditText)findViewById(R.id.description_notification);
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef = database.getReference().child("Notices");
+
+                myRef.child("Notice1").child("title").setValue(notification_title.getText().toString());
+                myRef.child("Notice1").child("description").setValue(notification_description.getText().toString());
+            }
+        });
+
+
+
+
+    }
+}

70bd70a3819759226951409794ff552f423a42f4
billion-yash <billionyash@gmail.com>
2020-12-17 19:27:17 +0530

    login updted in host app

---
+++ gcekhost/src/main/java/com/example/gcekhost/login/Admin.java
@@ -0,0 +1,13 @@
+package com.example.gcekhost.login;
+
+public class Admin {
+    public String Password;
+    public String Branch;
+    public String Name;
+
+    Admin(String password , String branch , String name){
+        Password = password;
+        Branch = branch;
+        Name = name;
+    }
+}
--- gcekhost/src/main/java/com/example/gcekhost/login/Login_page.java
+++ gcekhost/src/main/java/com/example/gcekhost/login/Login_page.java
@@ -1,32 +1,31 @@
 package com.example.gcekhost.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.gcekhost.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.gcekhost.R;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login_page extends AppCompatActivity {
-    private FirebaseAuth mAuth;
     public EditText email ,password;
     public Button loginbtn;
 //    public TextView register_yourself;
     public ProgressDialog pb;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,35 +36,81 @@ public class Login_page extends AppCompatActivity {
         loginbtn = (Button)findViewById(R.id.loginbtn);
         pb = new ProgressDialog(this);
         Context context = getApplicationContext();
-        mAuth = FirebaseAuth.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pb.show();
-                mAuth.signInWithEmailAndPassword(email.getText().toString() , password.getText().toString()).
-                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                if(email.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext() , "Please enter valid email" , Toast.LENGTH_LONG).show();
+                }
+                else {
+                    pb.show();
+                    String memail = email.getText().toString();
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                        db.collection("AdminLogin").document(memail).get()
+                                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        if(documentSnapshot.exists()){
+                                            Log.d("yash", documentSnapshot.getData().toString());
+
+                                            String a = documentSnapshot.get("Password").toString();
+                                            Log.d("yash", a);
+                                        }
+                                        else {
+                                            Toast.makeText(getApplicationContext() , "Plese enter valid email" , Toast.LENGTH_LONG).show();
+                                        }
+                                        pb.dismiss();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(mAuth.getCurrentUser().isEmailVerified()){
-                                    pb.dismiss();
-                                    startActivity(new Intent(getApplicationContext() , MainActivity.class));
-                                }
-
-                            else {
+                            public void onFailure(@NonNull Exception e) {
                                 pb.dismiss();
-                                startActivity(new Intent(getApplicationContext() , Register_page.class));
-                            }}
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext() ,e.getMessage() ,Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(getApplicationContext() , Register_page.class));
-                    }
-                });
-
-
+                                Toast.makeText(getApplicationContext() , "Plese enter valid email" , Toast.LENGTH_LONG);
+                            }
+                        });
+                }
             }
         });
+
+
+
+
+
+
+
+//        mAuth = FirebaseAuth.getInstance();
+//        loginbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                pb.show();
+//                mAuth.signInWithEmailAndPassword(email.getText().toString() , password.getText().toString()).
+//                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if(mAuth.getCurrentUser().isEmailVerified()){
+//                                    pb.dismiss();
+//                                    startActivity(new Intent(getApplicationContext() , MainActivity.class));
+//                                }
+//
+//                            else {
+//                                pb.dismiss();
+//                                startActivity(new Intent(getApplicationContext() , Register_page.class));
+//                            }}
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext() ,e.getMessage() ,Toast.LENGTH_LONG).show();
+//                        startActivity(new Intent(getApplicationContext() , Register_page.class));
+//                    }
+//                });
+//
+//
+//            }
+//        });
 //        register_yourself.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {

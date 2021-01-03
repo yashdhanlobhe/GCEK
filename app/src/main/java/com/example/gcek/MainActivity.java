167c4119b36573166774ff5efc3e07bc1a942393
billion-yash <billionyash@gmail.com>
2021-01-03 12:02:59 +0530

    added navigation menu and drawer

--- app/src/main/java/com/example/gcek/MainActivity.java
+++ app/src/main/java/com/example/gcek/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.gcek;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    NotificationManagerCompat notificationManagerCompat;
+
+    DrawerLayout drawerLayout ;
+    ActionBarDrawerToggle toggleButton ;
+    NavigationView navigationView ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        notificationManagerCompat = NotificationManagerCompat.from(this);
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        navigationView = (NavigationView)findViewById(R.id.main_nav_view);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+
+        toggleButton = new ActionBarDrawerToggle(this , drawerLayout , toolbar , R.string.open , R.string.close);
+        toggleButton.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext() , "home" , Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_notifications:
+                        Toast.makeText(getApplicationContext() , "notification" , Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_setting:
+                        Toast.makeText(getApplicationContext() , "setting" , Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return  false;
+            }
+        });
+
     }
 }

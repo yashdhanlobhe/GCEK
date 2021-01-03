c52068d81696adb9b0e85cf9373f87a9f0ad7f93
billion-yash <billionyash@gmail.com>
2021-01-03 17:26:21 +0530

    done with fragment home setting home and Notification_fragment

--- app/src/main/java/com/example/gcek/MainActivity.java
+++ app/src/main/java/com/example/gcek/MainActivity.java
@@ -7,14 +7,19 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.Toolbar;
 
+import com.example.gcek.maindrawer.HomeFragment;
+import com.example.gcek.maindrawer.Notification_fragment;
+import com.example.gcek.maindrawer.settingfragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout ;
     ActionBarDrawerToggle toggleButton ;
     NavigationView navigationView ;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +35,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
+
         navigationView = (NavigationView)findViewById(R.id.main_nav_view);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+        frameLayout= findViewById(R.id.main_frame_layout);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout , new HomeFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_home);
 
         toggleButton = new ActionBarDrawerToggle(this , drawerLayout , toolbar , R.string.open , R.string.close);
         toggleButton.syncState();
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment temp ;
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 switch (item.getItemId()){
                     case R.id.nav_home:
-                        Toast.makeText(getApplicationContext() , "home" , Toast.LENGTH_LONG).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
+                        temp = new HomeFragment();
                         break;
                     case R.id.nav_notifications:
-                        Toast.makeText(getApplicationContext() , "notification" , Toast.LENGTH_LONG).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
+                        temp = new Notification_fragment();
                         break;
                     case R.id.nav_setting:
-                        Toast.makeText(getApplicationContext() , "setting" , Toast.LENGTH_LONG).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
+                        temp = new settingfragment();
                         break;
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout , temp).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
                 return  false;
             }
         });

6ade0e948e1cbac4b4c06cb5605e838083bcf737
billion-yash <billionyash@gmail.com>
2021-01-03 08:38:07 +0530

    errors with sending notificaiton

---
+++ gcekhost/src/main/java/com/example/gcekhost/MyFirebaseMessagingService.java
@@ -0,0 +1,107 @@
+package com.example.gcekhost;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private final String ADMIN_CHANNEL_ID ="admin_channel";
+    private static final String TAG = "mFirebaseIIDService";
+    private static final String SUBSCRIBE_TO = "my-topic";
+    public String token;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG , "sTARTED");
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        token = task.getResult();
+                    }
+                });
+        FirebaseMessaging.getInstance().subscribeToTopic(SUBSCRIBE_TO);
+        Log.i(TAG, "onTokenRefresh completed with token: " + token);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        token = s;
+    }
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.i("started" ,"MyFirebaseMessagingService" );
+        final Intent intent = new Intent(this, MainActivity.class);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        int notificationID = new Random().nextInt(3000);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels(notificationManager);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this , 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
+                R.drawable.common_google_signin_btn_icon_dark);
+
+        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("message"))
+                .setAutoCancel(true)
+                .setSound(notificationSoundUri)
+                .setContentIntent(pendingIntent);
+        notificationManager.notify(notificationID, notificationBuilder.build());
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels(NotificationManager notificationManager){
+        CharSequence adminChannelName = "New notification";
+        String adminChannelDescription = "Device to devie notification";
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
+    }
+}
--- gcekhost/src/main/java/com/example/gcekhost/Notification/AddNotification.java
+++ gcekhost/src/main/java/com/example/gcekhost/Notification/AddNotification.java
@@ -1,42 +1,56 @@
 package com.example.gcekhost.Notification;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.gcekhost.App;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.gcekhost.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.sql.Time;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AddNotification extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference myRef;
     Button send_btn , seeNotifiacationBtn;
-    NotificationManagerCompat notificationManagerCompat;
-    //
+
     EditText notification_title , notification_description;
+
+    final private String FCM_API = "https://fcm.googleapis.com/fcm/send";
+    final private String serverKey = "key=" + "AAAAok9YObw:APA91bHgUXyZVXACilf2LCI0qb7SrBz22HLmIK0dzkw-GkO4i8rGGady1FSHy1Tm9NpW2t3C1dOHg_pJsguwWYrXFiel2mBbxWo5r0ChOlcdK-w6bg2xd3GgARyxxYkKA9vkx8YqNHE3";
+    final private String contentType = "application/json";
+    final String TAG = "NOTIFICATION TAG";
+
+    String NOTIFICATION_TITLE;
+    String NOTIFICATION_MESSAGE;
+    String TOPIC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addnotifcation);
-        // Write a message to the database
-
+        Log.i("add notification" , "add");
         database = FirebaseDatabase.getInstance();
-        notificationManagerCompat = NotificationManagerCompat.from(this);
-
         send_btn = (Button)findViewById(R.id.send_btn_notifiacaiton);
         seeNotifiacationBtn = (Button)findViewById(R.id.see_notifiaction_btn);
 
@@ -46,22 +60,12 @@ public class AddNotification extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String id =Calendar.getInstance().getTime().toString();
-
                 myRef = database.getReference().child("Notices").child(id);
-
                 myRef.child("title").setValue(notification_title.getText().toString());
                 myRef.child("description").setValue(notification_description.getText().toString());
                 myRef.child("id").setValue(id);
-                android.app.Notification notification  = new NotificationCompat.Builder(getApplicationContext() , App.CHANALE1ID)
-                        .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                        .setContentTitle(notification_title.getText().toString())
-                        .setContentText(notification_description.getText().toString())
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                        .build();
-                notificationManagerCompat.notify(1 , notification);
 
-
+                messenging(notification_title.getText().toString() , notification_description.getText().toString() );
             }
         });
         seeNotifiacationBtn.setOnClickListener(new View.OnClickListener() {
@@ -70,8 +74,85 @@ public class AddNotification extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext() , Notification.class));
             }
         });
+    }
+
+    private void messenging(String title , String message) {
+        TOPIC = "/topics/userABC";
+
+        JSONObject notification = new JSONObject();
+        JSONObject notifcationBody = new JSONObject();
+        try {
+            notifcationBody.put("title", NOTIFICATION_TITLE);
+            notifcationBody.put("message", NOTIFICATION_MESSAGE);
+
+            notification.put("to", TOPIC);
+            notification.put("data", notifcationBody);
+        } catch (JSONException e) {
+            Log.e(TAG, "onCreate: " + e.getMessage() );
+        }
+        sendNotification(notification);
+    }
+
+    private void sendNotification(JSONObject notification) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(FCM_API, notification,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i(TAG, "onResponse: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG, "onErrorResponse: Didn't work");
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization", serverKey);
+                params.put("Content-Type", contentType);
+                Log.i("Authorization", serverKey);
+                return params;
+
+            }
+        };
+        Log.i("debugyd","started");
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+    }
+}
 
 
+class MySingleton {
+    private  static MySingleton instance;
+    private RequestQueue requestQueue;
+    private Context ctx;
 
+    private MySingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new MySingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        Log.i("getRequestQueue" , "getRequestQueue");
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+
+        Log.i("added" , "Request queue");
+        getRequestQueue().add(req);
     }
 }

ece375fb9520a4790b18694fd460ad2c0916b8dd
billion-yash <billionyash@gmail.com>
2021-01-11 11:10:25 +0530

    uploading of user data and uploading data image is done adding spinner and uploading there data is remaining and the empty box detection is remaining

--- app/src/main/java/com/example/gcek/login/register_page.java
+++ app/src/main/java/com/example/gcek/login/register_page.java
@@ -7,8 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.UnicodeSetSpanner;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -24,9 +27,20 @@ import android.widget.Toast;
 import com.example.gcek.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class register_page extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
     EditText fullname , rollNo , email , phoneNO , password;
@@ -36,10 +50,119 @@ public class register_page extends AppCompatActivity implements AdapterView.OnIt
     Button sendmail;
     int PICKimg=100;
     Uri uri ;
+    Context mcontext;
+    ProgressDialog pb;
+    String tag = "RegisterPAGE";
+    FirebaseFirestore db;
+    StorageReference mStorageRef;
+    Uri profileImageUri;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+        mcontext = this;
+        intiUi();
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+        sendmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        mAuth = FirebaseAuth.getInstance();
+        pb.show();
+        mAuth.createUserWithEmailAndPassword(email.getText().toString() , password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        mAuth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                UploadImageToFireStore(uri);
+                                pb.dismiss();
+                                Log.d(tag , "ADDED USER SUCCESSFULLY");
+                                Toast.makeText(mcontext , "Registration successful plese Verify Your mail" , Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                    pb.dismiss();
+                    Log.d(tag , e.getMessage());
+                    Toast.makeText(mcontext , e.getMessage() , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void UploadImageToFireStore(Uri uri) {
+
+        StorageReference profileImageRef = mStorageRef.child("images/UsersProfile/"+email.getText().toString()+".jpg");
+
+        profileImageRef.putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+
+                        profileImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                profileImageUri = uri;
+                                Log.d(tag , uri.toString());
+                                uploadData();
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+    private void uploadData() {
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("Name", fullname.getText().toString());
+        user.put("GCEKID", rollNo.getText().toString());
+        user.put("PhoneNo", phoneNO.getText().toString());
+        user.put("ProfileImage" , profileImageUri.toString());
+
+        db.collection("StudentUsers").document(email.getText().toString())
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("yd22", "DocumentSnapshot added with ID: ");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("yd22", "Error adding document", e);
+                    }
+                });
+    }
+
+    private void intiUi() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
         fullname = (EditText)findViewById(R.id.fullnameregister);
         email = (EditText)findViewById(R.id.emailregister);
         rollNo = (EditText)findViewById(R.id.idregister);
@@ -49,44 +172,8 @@ public class register_page extends AppCompatActivity implements AdapterView.OnIt
         passout=(Spinner)findViewById(R.id.passoutregister);
         imageView=(ImageView)findViewById(R.id.imgregisterlay);
         sendmail = (Button)findViewById(R.id.buttonRegister);
-
         mAuth =  FirebaseAuth.getInstance();
-        ProgressDialog pb = new ProgressDialog(getApplicationContext());
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                OpenGallery();
-            }
-        });
-
-        sendmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth = FirebaseAuth.getInstance();
-//                pb.show();
-                mAuth.createUserWithEmailAndPassword(email.getText().toString() , password.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-//                                        pb.dismiss();
-                                        startActivity(new Intent(getApplicationContext() , login_page.class));
-                                        Log.d("yd22" , "ADDED USER SUCCESSFULLY");
-
-                                    }
-                                });
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                       Log.d("yd22" , e.getMessage());
-                    }
-                });
-            }
-        });
+        pb = new ProgressDialog(mcontext);
     }
 
     private void OpenGallery() {

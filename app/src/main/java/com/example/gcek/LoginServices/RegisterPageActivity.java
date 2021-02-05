11ec0ef3756d651109d87101058ee8c82e29b657
billion-yash <billionyash@gmail.com>
2021-02-05 18:07:23 +0530

    created Working yd branch

--- app/src/main/java/com/example/gcek/LoginServices/RegisterPageActivity.java
+++ app/src/main/java/com/example/gcek/LoginServices/RegisterPageActivity.java
@@ -36,6 +36,7 @@ import static com.example.gcek.Services.CompressImage.compressimage;
 
 public class RegisterPageActivity extends AppCompatActivity  {
     //working yd
+
     EditText fullname , rollNo , email , phoneNO , password;
     ImageView imageView;
     Button sendmail;
